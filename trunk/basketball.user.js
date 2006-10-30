115ed0a7970a18bda36a16b7f16e5ba1f23b83e6
robobruin <robobruin@11e3c3e4-fc19-0410-be34-8bb59711dbe6>
2006-10-30 00:32:35 +0000

    modal enhancements

--- trunk/basketball.user.js
+++ trunk/basketball.user.js
@@ -18,19 +18,24 @@ The script is set to run on all urls that match
 http://basketball.fantasysports.yahoo.com/*
 So if you're not on you daily stats page, disable it by right-clicking
 on the monkey in the lower right and unchecking the script.
+
+credits:-
+http://gabrito.com/files/subModal/
+http://www.sitening.com/blog/2006/03/29/create-a-modal-dialog-using-css-and-javascript/
+
 */
 
 (function () {
 
     var HIDDEN_DIV_ID = 'robobruinDiv';
-    var BATTER = 'batter';
-    var PITCHER = 'pitcher';
+    var MODAL_DIV_ID = 'robobruinModal';
+    var STAT_BODY_ID = 'robobruinTableBody';
 
     function xpath(doc, xpath) {
         return doc.evaluate(xpath, doc, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
     }
 
-    function getDocument(url, playerId, homeStatColumn, offset, playerType) {
+    function getDocument(url, playerId) {
         GM_xmlhttpRequest({
             method:"GET",
             url:url,
@@ -43,13 +48,13 @@ on the monkey in the lower right and unchecking the script.
                 s = s.replace(/\n/g, ' ');
                 s = s.replace(/^.*<body[^>]*>(.*)<\/body>.*$/gi, "$1");
 
-                var stats = processBoxscore(s, playerId, playerType);
-                printStats(homeStatColumn, stats, offset, playerType);
+                var stats = processBoxscore(s, playerId);
+                printStats(stats);
             }
         });
     }
 
-    function processBoxscore(html, playerId, playerType) {
+    function processBoxscore(html, playerId) {
         var div = document.getElementById(HIDDEN_DIV_ID);
         if (!div) {
             div = document.createElement("div");
@@ -69,59 +74,65 @@ on the monkey in the lower right and unchecking the script.
         } else {
             stats = new Object();
             var boxScoreStatColumn = nodes.snapshotItem(i - 1).parentNode.parentNode.childNodes;
+            var playerName = nodes.snapshotItem(i - 1).text.replace('.', '');
 
+            stats.HPAB = boxScoreStatColumn[7].innerHTML + '/' + boxScoreStatColumn[3].innerHTML;
+            stats.R = boxScoreStatColumn[5].innerHTML;
+            stats.RBI = boxScoreStatColumn[9].innerHTML;
 
-                var playerName = nodes.snapshotItem(i - 1).text.replace('.', '');
-
-                stats.HR = getHRorSB('HR', playerName, document);
-                stats.SB = getHRorSB('SB', playerName, document);
-                stats.HPAB = boxScoreStatColumn[7].innerHTML + '/' + boxScoreStatColumn[3].innerHTML;
-                stats.R = boxScoreStatColumn[5].innerHTML;
-                stats.RBI = boxScoreStatColumn[9].innerHTML;
-
-                if (boxScoreStatColumn[3].innerHTML * 1 > 0) {
-                    var avg = (boxScoreStatColumn[7].innerHTML * 1 / boxScoreStatColumn[3].innerHTML * 1).toFixed(3);
-                    stats.AVG = (new String(avg).charAt(0) != '1') ? (avg.substring(1, avg.length)) : avg;
-                } else {
-                    stats.AVG = '-';
-                }
             return stats;
         }
     }
 
-    function getHRorSB(type, playerName, document) {
-        var nodes = xpath(document, "//td[@class='yspscores' and contains(.,'" + type + "') and contains(.,'" + playerName + "')]");
-        var j = nodes.snapshotLength;
-        var numStat = 0;
-
-        if (j) {
-            var statLine = nodes.snapshotItem(j - 1).textContent;
-            var re = new RegExp(".*(" + playerName + " *\\d?).+", 'gi');
-            statLine = statLine.replace(re, "$1");
-            statLine = statLine.replace(/[^\d]+/, '');
-            numStat = (statLine == '') ? 1 : statLine;
+    function printStats(stats) {
+        var statBody = document.getElementById(STAT_BODY_ID);
+        if (!statBody) {
+            statBody = addModalOverlay();
         }
-        return numStat;
-    }
 
-
-    function printStats(homeStatColumn, stats, offset, playerType) {
+        var tr = null;
         if (stats) {
-            // H/AB
-            homeStatColumn[(8 + offset * 2)].innerHTML = stats.HPAB;
-            // R
-            homeStatColumn[(10 + offset * 2)].innerHTML = stats.R;
-            // HR
-            homeStatColumn[(12 + offset * 2)].innerHTML = stats.HR;
-            // RBI
-            homeStatColumn[(14 + offset * 2)].innerHTML = stats.RBI;
-            // sb
-            homeStatColumn[(16 + offset * 2)].innerHTML = stats.SB;
-            //avg
-            homeStatColumn[(18 + offset * 2)].innerHTML = stats.AVG;
+            tr = document.createElement("tr").appendChild(document.createElement("td").appendChild(document.createTextNode(stats.name)));
+        }
+
+        if (tr) {
+            statBody.appendChild(tr);
         }
     }
 
+    function showOverlay() {
+        var div = document.getElementById(MODAL_DIV_ID);
+        div.style.visibility = (div.style.visibility == "visible") ? "hidden" : "visible";
+    }
+
+    function addModalOverlay() {
+        var div = document.createElement("div");
+        document.body.appendChild(div);
+        div.id = MODAL_DIV_ID;
+
+        div.innerHTML =
+        '<div><table>' +
+            '<tbody id="'+ STAT_BODY_ID+'">' +
+            '</tbody>' +
+        '</table>' +
+        "</div>";
+
+        addGlobalStyle('#' + MODAL_DIV_ID + " {visibility: hidden;position: absolute;left: 0px;top: 0px;width:100%;height:100%;text-align:center;z-index: 200;background: url(\"data:image/png,%89PNG%0D%0A%1A%0A%00%00%00%0DIHDR%00%00%002%00%00%002%01%03%00%00%00%24%F1%1A%F2%00%00%00%06PLTE%9D%BF%C4%FF%FF%FFo%99%7C%D4%00%00%00%02tRNS%FF%00%E5%B70J%00%00%00%01bKGD%01%FF%02-%DE%00%00%00%09pHYs%00%00%00H%00%00%00H%00F%C9k%3E%00%00%00yIDATx%01%05%C1%01%01%00%00%08%02%20%1C%D9I%07u%A2%13A%06%5C%0A6%03.%05%9B%01%97%82%CD%80K%C1f%C0%A5%603%E0R%B0%19p)%D8%0C%B8%14l%06%5C%0A6%03.%05%9B%01%97%82%CD%80K%C1f%C0%A5%603%E0R%B0%19p)%D8%0C%B8%14l%06%5C%0A6%03.%05%9B%01%97%82%CD%80K%C1f%C0%A5%603%E0R%B0%19p)%D8%0C%B8%14l%06%5C%0A%F6%01%90%ADD%F3%BDe%02%17%00%00%00%00IEND%AEB%60%82\");}");
+        addGlobalStyle('#' + MODAL_DIV_ID + ' div {width:700px;margin: 100px auto;background-color:#fff;border:1px solid #000;padding:15px;text-align:center;z-index:201;}');
+        showOverlay();
+        centerPopWin();
+
+        return document.getElementById(STAT_BODY_ID);
+    }
+
+    function addGlobalStyle(css) {
+        var style = document.createElement("style");
+	    style.type = "text/css";
+        style.innerHTML = css;
+        document.getElementsByTagName('head')[0].appendChild(style);
+    }
+
+    /*
     function processFantasyHome(nodes) {
         for (var i = 0; i < nodes.snapshotLength; i++) {
             var userIdNode = nodes.snapshotItem(i);
@@ -136,14 +147,90 @@ on the monkey in the lower right and unchecking the script.
                 boxscoreLink = new String(boxscoreLink).replace('recap', 'boxscore');
                 statColumn[(6 + offset * 2)].childNodes[0].setAttribute("href", boxscoreLink);
 
-                getDocument(boxscoreLink,
-                        playerId,
-                        statColumn,
-                        offset, playerType);
+                getDocument(boxscoreLink, playerId);
             }
         }
     }
+    */
+    function getViewportHeight() {
+        if (window.innerHeight!=window.undefined) return window.innerHeight;
+        if (document.compatMode=='CSS1Compat') return document.documentElement.clientHeight;
+        if (document.body) return document.body.clientHeight;
+        return window.undefined;
+    }
+
+    function getViewportWidth() {
+        if (window.innerWidth!=window.undefined) return window.innerWidth;
+        if (document.compatMode=='CSS1Compat') return document.documentElement.clientWidth;
+        if (document.body) return document.body.clientWidth;
+        return window.undefined;
+    }
+
+    function centerPopWin() {
+        var popMask = document.getElementById(MODAL_DIV_ID);
+
+        var width = popMask.offsetWidth;
+        var height = popMask.offsetHeight;
+
+        var fullHeight = getViewportHeight();
+        var fullWidth = getViewportWidth();
+        var scLeft,scTop;
+        if (self.pageYOffset) {
+            scLeft = self.pageXOffset;
+            scTop = self.pageYOffset;
+        } else if (document.documentElement && document.documentElement.scrollTop) {
+            scLeft = document.documentElement.scrollLeft;
+            scTop = document.documentElement.scrollTop;
+        } else if (document.body) {
+            scLeft = document.body.scrollLeft;
+            scTop = document.body.scrollTop;
+        }
+
+        popMask.style.height = fullHeight + "px";
+        popMask.style.width = fullWidth + "px";
+        popMask.style.top = scTop + "px";
+        popMask.style.left = scLeft + "px";
+
+        //check that user's screen is big enough for auto centering...
+        if (fullHeight > height) {
+            popMask.style.top = (scTop + ((fullHeight - height) / 2)) + "px";
+        }
+        if (fullWidth > width) {
+            popMask.style.left =  (scLeft + ((fullWidth - width) / 2)) + "px";
+        }
+    }
+
+    function addEvent(obj, evType, fn) {
+        if (obj.addEventListener) {
+            obj.addEventListener(evType, fn, false);
+            return true;
+        } else if (obj.attachEvent) {
+            var r = obj.attachEvent("on" + evType, fn);
+            return r;
+        } else {
+            return false;
+        }
+    }
+
+    addEvent(window, "resize", centerPopWin);
+    addEvent(window, "scroll", centerPopWin);
 
     var nodes = xpath(document, "//table[@id='statTable0']/tbody/tr/td[@class='player']/div/a[@href]");
     processFantasyHome(nodes);
+
+    function processFantasyHome(nodes) {
+        for (var i = 0; i < nodes.snapshotLength; i++) {
+            var userIdNode = nodes.snapshotItem(i);
+            var playerId = userIdNode.getAttribute('href');
+            playerId = playerId.replace(/[^0-9]/g, '');
+
+            var playerName = userIdNode.text;
+            var stats = new Object();
+            stats.name = playerName;
+            stats.id = playerId;
+
+            printStats(stats);
+        }
+    }
+
 })();
