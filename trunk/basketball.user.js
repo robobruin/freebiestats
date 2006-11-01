02bd3fb4722a7f61240c8d85b0a5e277b86ad5f3
robobruin <robobruin@11e3c3e4-fc19-0410-be34-8bb59711dbe6>
2006-11-01 05:13:24 +0000

    implemented live scoring in modal window

--- trunk/basketball.user.js
+++ trunk/basketball.user.js
@@ -47,9 +47,8 @@ http://www.sitening.com/blog/2006/03/29/create-a-modal-dialog-using-css-and-java
                 var s = new String(details.responseText);
                 s = s.replace(/\n/g, ' ');
                 s = s.replace(/^.*<body[^>]*>(.*)<\/body>.*$/gi, "$1");
-
-                var stats = processBoxscore(s, playerId);
-                printStats(stats);
+                var row = processBoxscore(s, playerId);
+                printStats(row);
             }
         });
     }
@@ -67,35 +66,22 @@ http://www.sitening.com/blog/2006/03/29/create-a-modal-dialog-using-css-and-java
         var nodes = xpath(document, "//tr[contains(@class,'ysprow')]/td/a[contains(@href,'" + playerId + "')]");
         var i = nodes.snapshotLength;
 
-        var stats = null;
-
         if (!i) {
-            return stats;
+            return null;
         } else {
-            stats = new Object();
-            var boxScoreStatColumn = nodes.snapshotItem(i - 1).parentNode.parentNode.childNodes;
-            var playerName = nodes.snapshotItem(i - 1).text.replace('.', '');
-
-            stats.HPAB = boxScoreStatColumn[7].innerHTML + '/' + boxScoreStatColumn[3].innerHTML;
-            stats.R = boxScoreStatColumn[5].innerHTML;
-            stats.RBI = boxScoreStatColumn[9].innerHTML;
-
-            return stats;
+            var node = nodes.snapshotItem(i - 1).parentNode.parentNode.cloneNode(true);
+            return node;
         }
     }
 
-    function printStats(stats) {
+    function printStats(row) {
         var statBody = document.getElementById(STAT_BODY_ID);
         if (!statBody) {
             statBody = addModalOverlay();
         }
 
-        var tr = null;
-        if (stats) {
-            tr = document.createElement("tr").appendChild(document.createElement("td").appendChild(document.createTextNode(stats.name)));
-        }
-
-        if (tr) {
+        if (row) {
+            var tr = document.createElement("tr").appendChild(row);
             statBody.appendChild(tr);
         }
     }
@@ -106,19 +92,19 @@ http://www.sitening.com/blog/2006/03/29/create-a-modal-dialog-using-css-and-java
     }
 
     function addModalOverlay() {
-        addListenerStatTracker();
         var div = document.createElement("div");
         document.body.appendChild(div);
         div.id = MODAL_DIV_ID;
 
         div.innerHTML =
-        '<div><table>' +
+        '<div><table cellpadding="3">' +
+            '<thead><tr><td width="18%" height="18" align="left">&nbsp;Name</td><td width="6%">Min</td><td width="8%">FG</td><td width="8%">3Pt</td><td width="8%">FT</td><td width="8%">Off</td><td width="6%">Reb</td><td width="6%">Ast</td><td width="6%">TO</td><td width="6%">Stl</td><td width="6%">Blk</td><td width="6%">PF</td><td width="8%">Pts&nbsp;</td></tr></thead>' +
             '<tbody id="'+ STAT_BODY_ID+'">' +
             '</tbody>' +
         '</table><a id="roboClose" href="#">close</a></div>';
 
-        addGlobalStyle('#' + MODAL_DIV_ID + " {visibility: hidden;position: absolute;left: 0px;top: 0px;width:100%;height:100%;text-align:center;z-index: 200;background: url(\"data:image/png,%89PNG%0D%0A%1A%0A%00%00%00%0DIHDR%00%00%002%00%00%002%01%03%00%00%00%24%F1%1A%F2%00%00%00%06PLTE%9D%BF%C4%FF%FF%FFo%99%7C%D4%00%00%00%02tRNS%FF%00%E5%B70J%00%00%00%01bKGD%01%FF%02-%DE%00%00%00%09pHYs%00%00%00H%00%00%00H%00F%C9k%3E%00%00%00yIDATx%01%05%C1%01%01%00%00%08%02%20%1C%D9I%07u%A2%13A%06%5C%0A6%03.%05%9B%01%97%82%CD%80K%C1f%C0%A5%603%E0R%B0%19p)%D8%0C%B8%14l%06%5C%0A6%03.%05%9B%01%97%82%CD%80K%C1f%C0%A5%603%E0R%B0%19p)%D8%0C%B8%14l%06%5C%0A6%03.%05%9B%01%97%82%CD%80K%C1f%C0%A5%603%E0R%B0%19p)%D8%0C%B8%14l%06%5C%0A%F6%01%90%ADD%F3%BDe%02%17%00%00%00%00IEND%AEB%60%82\");}");
-        addGlobalStyle('#' + MODAL_DIV_ID + ' div {width:700px;margin: 100px auto;background-color:#fff;border:1px solid #000;padding:15px;text-align:center;z-index:201;}');
+        GM_addStyle('#' + MODAL_DIV_ID + " {visibility: hidden;position: absolute;left: 0px;top: 0px;width:100%;height:100%;text-align:center;z-index: 200;background: url(\"data:image/png,%89PNG%0D%0A%1A%0A%00%00%00%0DIHDR%00%00%002%00%00%002%01%03%00%00%00%24%F1%1A%F2%00%00%00%06PLTE%9D%BF%C4%FF%FF%FFo%99%7C%D4%00%00%00%02tRNS%FF%00%E5%B70J%00%00%00%01bKGD%01%FF%02-%DE%00%00%00%09pHYs%00%00%00H%00%00%00H%00F%C9k%3E%00%00%00yIDATx%01%05%C1%01%01%00%00%08%02%20%1C%D9I%07u%A2%13A%06%5C%0A6%03.%05%9B%01%97%82%CD%80K%C1f%C0%A5%603%E0R%B0%19p)%D8%0C%B8%14l%06%5C%0A6%03.%05%9B%01%97%82%CD%80K%C1f%C0%A5%603%E0R%B0%19p)%D8%0C%B8%14l%06%5C%0A6%03.%05%9B%01%97%82%CD%80K%C1f%C0%A5%603%E0R%B0%19p)%D8%0C%B8%14l%06%5C%0A%F6%01%90%ADD%F3%BDe%02%17%00%00%00%00IEND%AEB%60%82\");}");
+        GM_addStyle('#' + MODAL_DIV_ID + ' div {width:700px;margin: 100px auto;background-color:#fff;border:1px solid #000;padding:15px;text-align:center;z-index:201;}');
         showOverlay();
 
         var close = document.getElementById('roboClose');
@@ -129,45 +115,25 @@ http://www.sitening.com/blog/2006/03/29/create-a-modal-dialog-using-css-and-java
         return document.getElementById(STAT_BODY_ID);
     }
 
-    function addListenerStatTracker() {
-        var nodes = xpath(document, "//a[contains(@target, 'stattracker')]");
-        if (nodes.snapshotLength) {
-            var a = nodes.snapshotItem(0);
-            a.innerHTML = 'Launch FreebieStats!';
-            a.eventListener=null;
-            a.addEventListener('click',
-                           function(e) { showOverlay();return false; },
-                           false);
-        }
-    }
-
-    function addGlobalStyle(css) {
-        var style = document.createElement("style");
-	    style.type = "text/css";
-        style.innerHTML = css;
-        document.getElementsByTagName('head')[0].appendChild(style);
-    }
-
-    /*
     function processFantasyHome(nodes) {
         for (var i = 0; i < nodes.snapshotLength; i++) {
             var userIdNode = nodes.snapshotItem(i);
             var playerId = userIdNode.getAttribute('href');
             playerId = playerId.replace(/[^0-9]/g, '');
-
             var statColumn = userIdNode.parentNode.parentNode.parentNode.childNodes;
-            var offset = (statColumn.length == 22) ? 1 : 0;
+
+            var offset = (statColumn.length == 28) ? 1 : 0;
 
             if (statColumn[(6 + offset * 2)].childNodes[0].nodeName == 'A') {
                 var boxscoreLink = statColumn[(6 + offset * 2)].childNodes[0].getAttribute("href");
                 boxscoreLink = new String(boxscoreLink).replace('recap', 'boxscore');
                 statColumn[(6 + offset * 2)].childNodes[0].setAttribute("href", boxscoreLink);
-
                 getDocument(boxscoreLink, playerId);
             }
         }
+        addShowStatsButton();
     }
-    */
+
     function getViewportHeight() {
         if (window.innerHeight!=window.undefined) return window.innerHeight;
         if (document.compatMode=='CSS1Compat') return document.documentElement.clientHeight;
@@ -228,25 +194,22 @@ http://www.sitening.com/blog/2006/03/29/create-a-modal-dialog-using-css-and-java
         }
     }
 
+    function addShowStatsButton() {
+        var button = document.createElement("button");
+        document.body.appendChild(button);
+        button.id = 'robobruinStatBtn';
+        button.innerHTML = "Show Freebie Stats!";
+
+        button.addEventListener('click',
+                           function(e) { showOverlay(); },
+                           false);
+        GM_addStyle('#robobruinStatBtn {position:fixed;top:80px;left:80px;z-index:200;background-color:#0781C8;color:#fff;}');
+    }
+
     addEvent(window, "resize", centerPopWin);
     addEvent(window, "scroll", centerPopWin);
 
     var nodes = xpath(document, "//table[@id='statTable0']/tbody/tr/td[@class='player']/div/a[@href]");
     processFantasyHome(nodes);
-
-    function processFantasyHome(nodes) {
-        for (var i = 0; i < nodes.snapshotLength; i++) {
-            var userIdNode = nodes.snapshotItem(i);
-            var playerId = userIdNode.getAttribute('href');
-            playerId = playerId.replace(/[^0-9]/g, '');
-
-            var playerName = userIdNode.text;
-            var stats = new Object();
-            stats.name = playerName;
-            stats.id = playerId;
-
-            printStats(stats);
-        }
-    }
 
 })();
