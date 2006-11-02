fa95c38f71d18aeb0125b430208c6d793d23af6a
robobruin <robobruin@users.noreply.github.com>
2006-11-02 06:32:13 +0000

    bug fixes and styling changes

--- basketball.user.js
+++ basketball.user.js
@@ -35,7 +35,7 @@ http://www.sitening.com/blog/2006/03/29/create-a-modal-dialog-using-css-and-java
         return doc.evaluate(xpath, doc, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
     }
 
-    function getDocument(url, playerId) {
+    function getDocument(url, playerId, position) {
         GM_xmlhttpRequest({
             method:"GET",
             url:url,
@@ -48,7 +48,7 @@ http://www.sitening.com/blog/2006/03/29/create-a-modal-dialog-using-css-and-java
                 s = s.replace(/\n/g, ' ');
                 s = s.replace(/^.*<body[^>]*>(.*)<\/body>.*$/gi, "$1");
                 var row = processBoxscore(s, playerId);
-                printStats(row);
+                printStats(row, position);
             }
         });
     }
@@ -74,7 +74,7 @@ http://www.sitening.com/blog/2006/03/29/create-a-modal-dialog-using-css-and-java
         }
     }
 
-    function printStats(row) {
+    function printStats(row, position) {
         var statBody = document.getElementById(STAT_BODY_ID);
         if (!statBody) {
             statBody = addModalOverlay();
@@ -82,6 +82,11 @@ http://www.sitening.com/blog/2006/03/29/create-a-modal-dialog-using-css-and-java
 
         if (row) {
             var tr = document.createElement("tr").appendChild(row);
+            var className = (statBody.childNodes.length % 2 == 0) ? 'odd' : 'even';
+            tr.className = className;
+            if (position == 'BN') {
+                tr.className = 'bench';
+            }
             statBody.appendChild(tr);
         }
     }
@@ -97,7 +102,7 @@ http://www.sitening.com/blog/2006/03/29/create-a-modal-dialog-using-css-and-java
         div.id = MODAL_DIV_ID;
 
         div.innerHTML =
-        '<div><table cellpadding="3">' +
+        '<div><table id="roboTable" width="100%">' +
             '<thead><tr><td width="18%" height="18" align="left">&nbsp;Name</td><td width="6%">Min</td><td width="8%">FG</td><td width="8%">3Pt</td><td width="8%">FT</td><td width="8%">Off</td><td width="6%">Reb</td><td width="6%">Ast</td><td width="6%">TO</td><td width="6%">Stl</td><td width="6%">Blk</td><td width="6%">PF</td><td width="8%">Pts&nbsp;</td></tr></thead>' +
             '<tbody id="'+ STAT_BODY_ID+'">' +
             '</tbody>' +
@@ -105,6 +110,7 @@ http://www.sitening.com/blog/2006/03/29/create-a-modal-dialog-using-css-and-java
 
         GM_addStyle('#' + MODAL_DIV_ID + " {visibility: hidden;position: absolute;left: 0px;top: 0px;width:100%;height:100%;text-align:center;z-index: 200;background: url(\"data:image/png,%89PNG%0D%0A%1A%0A%00%00%00%0DIHDR%00%00%002%00%00%002%01%03%00%00%00%24%F1%1A%F2%00%00%00%06PLTE%9D%BF%C4%FF%FF%FFo%99%7C%D4%00%00%00%02tRNS%FF%00%E5%B70J%00%00%00%01bKGD%01%FF%02-%DE%00%00%00%09pHYs%00%00%00H%00%00%00H%00F%C9k%3E%00%00%00yIDATx%01%05%C1%01%01%00%00%08%02%20%1C%D9I%07u%A2%13A%06%5C%0A6%03.%05%9B%01%97%82%CD%80K%C1f%C0%A5%603%E0R%B0%19p)%D8%0C%B8%14l%06%5C%0A6%03.%05%9B%01%97%82%CD%80K%C1f%C0%A5%603%E0R%B0%19p)%D8%0C%B8%14l%06%5C%0A6%03.%05%9B%01%97%82%CD%80K%C1f%C0%A5%603%E0R%B0%19p)%D8%0C%B8%14l%06%5C%0A%F6%01%90%ADD%F3%BDe%02%17%00%00%00%00IEND%AEB%60%82\");}");
         GM_addStyle('#' + MODAL_DIV_ID + ' div {width:700px;margin: 100px auto;background-color:#fff;border:1px solid #000;padding:15px;text-align:center;z-index:201;}');
+        GM_addStyle('#roboTable {padding:3px;border-collapse:collapse;border: 1px solid #000;} tr.even {background-color:#f1f2ed;} thead tr {background-color:#ABAB9E;border-bottom:1px solid #000;} td {text-align:center;} tr.bench {background-color: #666;}');
         showOverlay();
 
         var close = document.getElementById('roboClose');
@@ -121,6 +127,8 @@ http://www.sitening.com/blog/2006/03/29/create-a-modal-dialog-using-css-and-java
             var playerId = userIdNode.getAttribute('href');
             playerId = playerId.replace(/[^0-9]/g, '');
             var statColumn = userIdNode.parentNode.parentNode.parentNode.childNodes;
+            var position = statColumn[0].innerHTML;
+
 
             var offset = (statColumn.length == 28) ? 1 : 0;
 
@@ -128,7 +136,7 @@ http://www.sitening.com/blog/2006/03/29/create-a-modal-dialog-using-css-and-java
                 var boxscoreLink = statColumn[(6 + offset * 2)].childNodes[0].getAttribute("href");
                 boxscoreLink = new String(boxscoreLink).replace('recap', 'boxscore');
                 statColumn[(6 + offset * 2)].childNodes[0].setAttribute("href", boxscoreLink);
-                getDocument(boxscoreLink, playerId);
+                getDocument(boxscoreLink, playerId, position);
             }
         }
         addShowStatsButton();
@@ -209,7 +217,7 @@ http://www.sitening.com/blog/2006/03/29/create-a-modal-dialog-using-css-and-java
     addEvent(window, "resize", centerPopWin);
     addEvent(window, "scroll", centerPopWin);
 
-    var nodes = xpath(document, "//table[@id='statTable0']/tbody/tr/td[@class='player']/div/a[@href]");
+    var nodes = xpath(document, "//table[@id='statTable0']/tbody/tr/td[@class='player']/div/a[@class='name' and @href]");
     processFantasyHome(nodes);
 
 })();
