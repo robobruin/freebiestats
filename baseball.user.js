83ca5a0a07c055df5b201029aba57deb4c904238
robobruin <robobruin@users.noreply.github.com>
2007-04-19 20:23:51 +0000

    code clean up and documentation, Issue 3

--- baseball.user.js
+++ baseball.user.js
@@ -13,36 +13,42 @@ http://code.google.com/p/freebiestats/issues/list
 
 Why does baseball rock? http://robobruin.blogspot.com/2007/02/fresh-start.html
 
-*--!Important!--*
-The script is set to run on all urls that match
-http://baseball.fantasysports.yahoo.com/*
-So if you're not on you daily stats page, disable it by right-clicking
-on the monkey in the lower right and unchecking the script.
-
 credits for modal dialog inspiration:
 http://gabrito.com/files/subModal/
 http://www.sitening.com/blog/2006/03/29/create-a-modal-dialog-using-css-and-javascript/
-
-Updates
-BugTraq: http://code.google.com/p/freebiestats/issues/list
-2007-04-18: Fixed Issues 4 and 5. Reported by Ethan Herbertson
-
 */
 
 (function () {
     if (!location.href.match(/^http\:\/\/baseball\.fantasysports\.yahoo\.com\/b\d\/\d+\/\d+.*/i)) {
         return;
     }
+    //Globals
     var HIDDEN_DIV_ID = 'robobruinDiv';
     var MODAL_DIV_ID = 'robobruinModal';
     var STAT_BODY_ID = 'robobruinTableBody';
     var BATTER = 'batter';
     var PITCHER = 'pitcher';
 
+    /**
+     Find all nodes matching an xpath expression
+     @doc document
+     @xpath xpath expression
+
+     @return in-order resulting nodes
+     */
     function xpath(doc, xpath) {
         return doc.evaluate(xpath, doc, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
     }
 
+    /**
+     Wrap greasemonkey's ajax request
+     @url                page to request
+     @playerId           id of the player to search for
+     @position           position of the player
+     @pitcherOrBatter    BATTER or PITCHER global
+
+     @return no return value
+     */
     function getDocument(url, playerId, position, pitcherOrBatter) {
         GM_xmlhttpRequest({
             method:"GET",
@@ -57,11 +63,17 @@ BugTraq: http://code.google.com/p/freebiestats/issues/list
                 s = s.replace(/^.*<style.*<\/style>/gi, ' ');
                 s = s.replace(/^.*<body[^>]*>(.*)<\/body>.*$/gi, "$1");
                 var document = appendToDocument(s);
-                printStats(document, playerId, position, pitcherOrBatter);
+                calculateStats(document, playerId, position, pitcherOrBatter);
             }
         });
     }
 
+    /**
+     Add hidden content to the DOM so we can run xpath on it
+     @html the raw html that will be part of the DOM
+
+     @return current document
+     */
     function appendToDocument(html) {
         var div = document.getElementById(HIDDEN_DIV_ID);
         if (!div) {
@@ -70,11 +82,19 @@ BugTraq: http://code.google.com/p/freebiestats/issues/list
             div.id = HIDDEN_DIV_ID;
             div.style.display = 'none';
         }
-
         div.innerHTML = html;
         return document;
     }
 
+    /**
+     Find a player's stat row from the boxscore based on their playerId.
+     We can use playerId since their name is hyperlinked and that contains the playerId.
+     Return the last match to insure we don't get a pitcher's batting stats.
+     @document document to parse
+     @playerId player id to search for
+
+     @return null if no matches, or the last node that matched.
+     */
     function processBoxscore(document, playerId) {
         var nodes = xpath(document, "//tr[contains(@class,'ysprow')]/td/a[contains(@href,'" + playerId + "')]");
         var i = nodes.snapshotLength;
@@ -87,6 +107,15 @@ BugTraq: http://code.google.com/p/freebiestats/issues/list
         }
     }
 
+    /**
+     HR and SB stats appear at the bottom of the boxscore table.
+     We need to do a string match on the player since the playerId does not appear.
+     @type SB or HR
+     @playerName name of player
+     @document document to parse
+
+     @return number of HR or SB
+     */
     function getHRorSB(type, playerName, document) {
         var nodes = xpath(document, "//td[@class='yspscores' and contains(.,'" + type + "') and contains(.,'" + playerName + "')]");
         var j = nodes.snapshotLength;
@@ -94,7 +123,7 @@ BugTraq: http://code.google.com/p/freebiestats/issues/list
 
         if (j) {
             var statLine = nodes.snapshotItem(j - 1).textContent;
-            //does the name appear in the catcher field?
+            //Remove everything in parentheses since that may create false matches
             statLine = statLine.replace(/\([^\)].+?\)/gi,'');
             if (statLine.indexOf(playerName) > -1) {
                 var re = new RegExp(".*(" + playerName + " *\\d?).+", 'gi');
@@ -106,7 +135,16 @@ BugTraq: http://code.google.com/p/freebiestats/issues/list
         return numStat;
     }
 
-    function printStats(document, playerId, position, pitcherOrBatter) {
+    /**
+     Calculates and appends a player's stats to the resulting table.
+     @document current document
+     @playerId player's ID
+     @position position of current player
+     @pitcherOrBatter PITCHER or BATTER
+
+     @return no return value
+     */
+    function calculateStats(document, playerId, position, pitcherOrBatter) {
         var row = processBoxscore(document, playerId);
 
         if (row) {
@@ -178,21 +216,36 @@ BugTraq: http://code.google.com/p/freebiestats/issues/list
         }
     }
 
-    function showOverlay() {
-        var div = document.getElementById(MODAL_DIV_ID);
-        div.style.visibility = (div.style.visibility == "visible") ? "hidden" : "visible";
+    /**
+     Enable or disable the modal window overlay
+     */
+    function removeOverlay() {
+        if (document.getElementById(MODAL_DIV_ID)) {
+            document.body.removeChild(document.getElementById(MODAL_DIV_ID));
+        }
     }
 
+    /**
+     Setup the modal window
+     */
     function setUpModal() {
+        if (document.getElementById(MODAL_DIV_ID)) {
+            document.body.removeChild(document.getElementById(MODAL_DIV_ID));
+        }        
         var div = document.createElement("div");
         div.appendChild(document.createElement("div"));
         document.body.appendChild(div);
         div.id = MODAL_DIV_ID;
-        GM_addStyle('#' + MODAL_DIV_ID + " {text-align:center;visibility: hidden;position: absolute;left: 0px;top: 0px;width:100%;height:100%;text-align:center;z-index: 200;background: url(\"data:image/png,%89PNG%0D%0A%1A%0A%00%00%00%0DIHDR%00%00%002%00%00%002%01%03%00%00%00%24%F1%1A%F2%00%00%00%06PLTE%9D%BF%C4%FF%FF%FFo%99%7C%D4%00%00%00%02tRNS%FF%00%E5%B70J%00%00%00%01bKGD%01%FF%02-%DE%00%00%00%09pHYs%00%00%00H%00%00%00H%00F%C9k%3E%00%00%00yIDATx%01%05%C1%01%01%00%00%08%02%20%1C%D9I%07u%A2%13A%06%5C%0A6%03.%05%9B%01%97%82%CD%80K%C1f%C0%A5%603%E0R%B0%19p)%D8%0C%B8%14l%06%5C%0A6%03.%05%9B%01%97%82%CD%80K%C1f%C0%A5%603%E0R%B0%19p)%D8%0C%B8%14l%06%5C%0A6%03.%05%9B%01%97%82%CD%80K%C1f%C0%A5%603%E0R%B0%19p)%D8%0C%B8%14l%06%5C%0A%F6%01%90%ADD%F3%BDe%02%17%00%00%00%00IEND%AEB%60%82\");}");
+        GM_addStyle('#' + MODAL_DIV_ID + " {text-align:center;position:absolute;left: 0px;top: 0px;width:100%;height:100%;text-align:center;z-index: 200;background: url(\"data:image/png,%89PNG%0D%0A%1A%0A%00%00%00%0DIHDR%00%00%002%00%00%002%01%03%00%00%00%24%F1%1A%F2%00%00%00%06PLTE%9D%BF%C4%FF%FF%FFo%99%7C%D4%00%00%00%02tRNS%FF%00%E5%B70J%00%00%00%01bKGD%01%FF%02-%DE%00%00%00%09pHYs%00%00%00H%00%00%00H%00F%C9k%3E%00%00%00yIDATx%01%05%C1%01%01%00%00%08%02%20%1C%D9I%07u%A2%13A%06%5C%0A6%03.%05%9B%01%97%82%CD%80K%C1f%C0%A5%603%E0R%B0%19p)%D8%0C%B8%14l%06%5C%0A6%03.%05%9B%01%97%82%CD%80K%C1f%C0%A5%603%E0R%B0%19p)%D8%0C%B8%14l%06%5C%0A6%03.%05%9B%01%97%82%CD%80K%C1f%C0%A5%603%E0R%B0%19p)%D8%0C%B8%14l%06%5C%0A%F6%01%90%ADD%F3%BDe%02%17%00%00%00%00IEND%AEB%60%82\");}");
         GM_addStyle('#' + MODAL_DIV_ID + ' div {width:500px;margin:100px auto;background-color:#fff;border:1px solid #000;padding:15px;text-align:center;z-index:201;}');
         GM_addStyle('.roboTable {width:100%;margin-bottom:20px;padding:3px;border-collapse:collapse;border: 1px solid #000;} tr.even {background-color:#f1f2ed;} thead tr {background-color:#ABAB9E;border-bottom:1px solid #000;} td {text-align:center;} tr.bench {background-color: #666;}');
+
+        addClose();
     }
 
+    /**
+     Add a close link to the modal
+     */
     function addClose() {
         var div = document.getElementById(MODAL_DIV_ID).childNodes[0];
         var close = document.createElement("a");
@@ -203,10 +256,13 @@ BugTraq: http://code.google.com/p/freebiestats/issues/list
 
         var closeLink = document.getElementById('roboClose');
         closeLink.addEventListener('click',
-                           function(e) { showOverlay(); },
+                           function(e) { removeOverlay(); },
                            false);
     }
 
+    /**
+
+    */
     function setUpTable(tableId, pitcherOrBatter) {
         var div = document.getElementById(MODAL_DIV_ID).childNodes[0];
         var table = document.createElement("table");
@@ -217,7 +273,6 @@ BugTraq: http://code.google.com/p/freebiestats/issues/list
                 '<thead><tr><td width="23%" height="18" align="left">&nbsp;Name</td><td width="8%">H/AB</td><td width="8%">R</td><td width="8%">HR</td><td width="8%">RBI</td><td width="8%">SB</td><td width="8%">AVG</td></tr></thead>' +
                 '<tbody id="'+ tableId+'">' +
                 '</tbody>';
-            showOverlay();
         } else {
             table.innerHTML +=
                 '<thead><tr><td width="23%" height="18" align="left">&nbsp;Name</td><td width="7%">IP</td><td width="7%">W</td><td width="7%">S</td><td width="7%">K</td><td width="7%">ERA</td><td width="7%">WHIP</td></tr></thead>' +
@@ -225,10 +280,18 @@ BugTraq: http://code.google.com/p/freebiestats/issues/list
                 '</tbody>';
         }
         div.appendChild(table);
-        if (pitcherOrBatter == BATTER) {addClose();}
+        //if (pitcherOrBatter == BATTER) {addClose();}
+        
         return document.getElementById(tableId);
     }
 
+    /**
+     From the daily management page, visit each boxscore link if it exists and fire off the processing.
+     @nodes              the player nodes
+     @pitcherOrBatter    PITCHER or BATTER
+
+     @returns no return value
+     */
     function processFantasyHome(nodes, pitcherOrBatter) {
         for (var i = 0; i < nodes.snapshotLength; i++) {
             var userIdNode = nodes.snapshotItem(i);
@@ -256,6 +319,9 @@ BugTraq: http://code.google.com/p/freebiestats/issues/list
         }
     }
 
+    /**
+     Modal window code
+     */
     function getViewportHeight() {
         if (window.innerHeight!=window.undefined) return window.innerHeight;
         if (document.compatMode=='CSS1Compat') return document.documentElement.clientHeight;
@@ -263,6 +329,9 @@ BugTraq: http://code.google.com/p/freebiestats/issues/list
         return window.undefined;
     }
 
+    /**
+     Modal window code
+     */
     function getViewportWidth() {
         if (window.innerWidth!=window.undefined) return window.innerWidth;
         if (document.compatMode=='CSS1Compat') return document.documentElement.clientWidth;
@@ -270,6 +339,9 @@ BugTraq: http://code.google.com/p/freebiestats/issues/list
         return window.undefined;
     }
 
+    /**
+     Modal window code
+     */
     function centerPopWin() {
         var popMask = document.getElementById(MODAL_DIV_ID);
 
@@ -304,6 +376,9 @@ BugTraq: http://code.google.com/p/freebiestats/issues/list
         }
     }
 
+    /**
+     Attach functions to events
+     */
     function addEvent(obj, evType, fn) {
         if (obj.addEventListener) {
             obj.addEventListener(evType, fn, false);
@@ -316,6 +391,9 @@ BugTraq: http://code.google.com/p/freebiestats/issues/list
         }
     }
 
+    /**
+     Add the blue show stats button
+     */
     function addShowStatsButton() {
         if (document.getElementById('robobruinStatBtn')) {
             return;
@@ -326,20 +404,30 @@ BugTraq: http://code.google.com/p/freebiestats/issues/list
         button.innerHTML = "Show Freebie Stats!";
 
         button.addEventListener('click',
-                           function(e) { showOverlay(); },
+                           function(e) {getStats();},
                            false);
         GM_addStyle('#robobruinStatBtn {position:fixed;top:80px;left:80px;z-index:200;background-color:#0781C8;color:#fff;}');
     }
 
-    addEvent(window, "resize", centerPopWin);
-    addEvent(window, "scroll", centerPopWin);
+    /**
+    Wrapper function to show stats.
+    */
+    function getStats() {
+        setUpModal();
+        var nodes = xpath(document, "//table[@id='statTable0']/tbody/tr/td[@class='player']/div/a[@class='name' and @href]");
+        processFantasyHome(nodes, BATTER);
 
-    setUpModal();
+        nodes = xpath(document, "//table[@id='statTable1']/tbody/tr/td[@class='player']/div/a[@class='name' and @href]");
+        processFantasyHome(nodes, PITCHER);
+        centerPopWin();
+        addEvent(window, "resize", centerPopWin);
+        addEvent(window, "scroll", centerPopWin);
+    }
 
-    var nodes = xpath(document, "//table[@id='statTable0']/tbody/tr/td[@class='player']/div/a[@class='name' and @href]");
-    processFantasyHome(nodes, BATTER);
-
-    nodes = xpath(document, "//table[@id='statTable1']/tbody/tr/td[@class='player']/div/a[@class='name' and @href]");
-    processFantasyHome(nodes, PITCHER);
-    centerPopWin();
+    addShowStatsButton();
 })();
+
+//Change Log
+//2007-04-18: Fixed Issues 4 and 5. Reported by Ethan Herbertson
+//2007-04-19: Code cleanup and documentation.  Lots of suggestions from Rodric Rabbah. Beginnings of issue 3. 
+
