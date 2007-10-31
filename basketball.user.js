b666b529720e1076e2a8c993e2ccd935fc0c1b5b
robobruin <robobruin@users.noreply.github.com>
2007-10-31 23:42:11 +0000

    temporary fix until I work out totals

--- basketball.user.js
+++ basketball.user.js
@@ -29,10 +29,43 @@ revision
 */
 
 (function () {
+    if (!location.href.match(/^http\:\/\/basketball\.fantasysports\.yahoo\.com\/nba\/\d+\/(team\?mid=)?\d+.*/i)) {
+        return;
+    }
 
-    var HIDDEN_DIV_ID = 'robobruinDiv';
-    var MODAL_DIV_ID = 'robobruinModal';
-    var STAT_BODY_ID = 'robobruinTableBody';
+    const HIDDEN_DIV_ID = 'robobruinDiv';
+    const MODAL_DIV_ID = 'robobruinModal';
+    const STAT_BODY_ID = 'robobruinTableBody';
+    const BUTTON_LABEL   = 'Show Freebie Stats!';
+
+    const STAT_BUTTON_ID = 'robobruinStatBtn';
+//
+//    function Baller() {
+//        this._playerId = '';
+//        this._name     = '';
+//        this._displayName = '';
+//        this._position = '';
+//        this._min = 0;
+//        this._fga = 0;
+//        this._fgm = 0;
+//        this._tpa = 0;
+//        this._tpm = 0;
+//        this._fta = 0;
+//        this._ftm = 0;
+//        this._oreb = 0;
+//        this._reb = 0;
+//        this._assists = 0;
+//        this._to = 0;
+//        this._steals = 0;
+//        this._blk = 0;
+//        this._pf = 0;
+//        this._pts = 0;
+//    }
+//    Baller.prototype.isOnBench   = function ()    {return (this._position == 'BN' || this._position == 'DL');}
+//
+//    var TotalBaller = new Baller();
+
+
 
     function xpath(doc, xpath) {
         return doc.evaluate(xpath, doc, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
@@ -49,6 +82,7 @@ revision
             onload:function(details) {
                 var s = new String(details.responseText);
                 s = s.replace(/\n/g, ' ');
+                s = s.replace(/^.*<style.*<\/style>/gi, ' ');
                 s = s.replace(/^.*<body[^>]*>(.*)<\/body>.*$/gi, "$1");
                 var row = processBoxscore(s, playerId);
                 printStats(row, position);
@@ -95,12 +129,11 @@ revision
         }
     }
 
-    function showOverlay() {
-        var div = document.getElementById(MODAL_DIV_ID);
-        div.style.visibility = (div.style.visibility == "visible") ? "hidden" : "visible";
-    }
-
     function addModalOverlay() {
+        if (document.getElementById(MODAL_DIV_ID)) {
+            document.body.removeChild(document.getElementById(MODAL_DIV_ID));
+        }
+
         var div = document.createElement("div");
         document.body.appendChild(div);
         div.id = MODAL_DIV_ID;
@@ -110,19 +143,35 @@ revision
             '<thead><tr><td width="18%" height="18" align="left">&nbsp;Name</td><td width="6%">Min</td><td width="8%">FG</td><td width="8%">3Pt</td><td width="8%">FT</td><td width="8%">Off</td><td width="6%">Reb</td><td width="6%">Ast</td><td width="6%">TO</td><td width="6%">Stl</td><td width="6%">Blk</td><td width="6%">PF</td><td width="8%">Pts&nbsp;</td></tr></thead>' +
             '<tbody id="'+ STAT_BODY_ID+'">' +
             '</tbody>' +
-        '</table><a id="roboClose" href="#">close</a></div>';
+        '</table></div>';
 
-        GM_addStyle('#' + MODAL_DIV_ID + " {visibility: hidden;position: absolute;left: 0px;top: 0px;width:100%;height:100%;text-align:center;z-index: 200;background: url(\"data:image/png,%89PNG%0D%0A%1A%0A%00%00%00%0DIHDR%00%00%002%00%00%002%01%03%00%00%00%24%F1%1A%F2%00%00%00%06PLTE%9D%BF%C4%FF%FF%FFo%99%7C%D4%00%00%00%02tRNS%FF%00%E5%B70J%00%00%00%01bKGD%01%FF%02-%DE%00%00%00%09pHYs%00%00%00H%00%00%00H%00F%C9k%3E%00%00%00yIDATx%01%05%C1%01%01%00%00%08%02%20%1C%D9I%07u%A2%13A%06%5C%0A6%03.%05%9B%01%97%82%CD%80K%C1f%C0%A5%603%E0R%B0%19p)%D8%0C%B8%14l%06%5C%0A6%03.%05%9B%01%97%82%CD%80K%C1f%C0%A5%603%E0R%B0%19p)%D8%0C%B8%14l%06%5C%0A6%03.%05%9B%01%97%82%CD%80K%C1f%C0%A5%603%E0R%B0%19p)%D8%0C%B8%14l%06%5C%0A%F6%01%90%ADD%F3%BDe%02%17%00%00%00%00IEND%AEB%60%82\");}");
+        GM_addStyle('#' + MODAL_DIV_ID + " {position: absolute;left: 0px;top: 0px;width:100%;height:100%;text-align:center;z-index: 200;background: url(\"data:image/png,%89PNG%0D%0A%1A%0A%00%00%00%0DIHDR%00%00%002%00%00%002%01%03%00%00%00%24%F1%1A%F2%00%00%00%06PLTE%9D%BF%C4%FF%FF%FFo%99%7C%D4%00%00%00%02tRNS%FF%00%E5%B70J%00%00%00%01bKGD%01%FF%02-%DE%00%00%00%09pHYs%00%00%00H%00%00%00H%00F%C9k%3E%00%00%00yIDATx%01%05%C1%01%01%00%00%08%02%20%1C%D9I%07u%A2%13A%06%5C%0A6%03.%05%9B%01%97%82%CD%80K%C1f%C0%A5%603%E0R%B0%19p)%D8%0C%B8%14l%06%5C%0A6%03.%05%9B%01%97%82%CD%80K%C1f%C0%A5%603%E0R%B0%19p)%D8%0C%B8%14l%06%5C%0A6%03.%05%9B%01%97%82%CD%80K%C1f%C0%A5%603%E0R%B0%19p)%D8%0C%B8%14l%06%5C%0A%F6%01%90%ADD%F3%BDe%02%17%00%00%00%00IEND%AEB%60%82\");}");
         GM_addStyle('#' + MODAL_DIV_ID + ' div {width:700px;margin: 100px auto;background-color:#fff;border:1px solid #000;padding:15px;text-align:center;z-index:201;}');
         GM_addStyle('#roboTable {padding:3px;border-collapse:collapse;border: 1px solid #000;} tr.even {background-color:#f1f2ed;} thead tr {background-color:#ABAB9E;border-bottom:1px solid #000;} td {text-align:center;} tr.bench {background-color: #666;}');
-        showOverlay();
-
-        var close = document.getElementById('roboClose');
-        close.addEventListener('click',
-                           function(e) { showOverlay(); },
-                           false);
-        centerPopWin();
+        addCloseAndRefresh();
         return document.getElementById(STAT_BODY_ID);
+    }
+
+        /**
+     Add a close link and refresh to the modal
+     */
+    function addCloseAndRefresh() {
+        var div = document.getElementById(MODAL_DIV_ID).childNodes[0];
+
+        var close = document.createElement("a");
+        close.id ="roboClose";
+        close.href = "#";
+        close.innerHTML = "[close]";
+        div.appendChild(close);
+        close.addEventListener('click', function(e) { removeOverlay(); }, false);
+
+        var refresh = document.createElement("a");
+        refresh.id = "roboRefresh";
+        refresh.href = "#";
+        refresh.innerHTML = "[refresh]";
+        refresh.addEventListener('click', function(e) { removeOverlay(); getStats();},false);
+        div.appendChild(refresh);
+        GM_addStyle('#roboRefresh {padding-left:10px;}');
     }
 
     function processFantasyHome(nodes) {
@@ -151,82 +200,43 @@ revision
         }
     }
 
-    function getViewportHeight() {
-        if (window.innerHeight!=window.undefined) return window.innerHeight;
-        if (document.compatMode=='CSS1Compat') return document.documentElement.clientHeight;
-        if (document.body) return document.body.clientHeight;
-        return window.undefined;
-    }
-
-    function getViewportWidth() {
-        if (window.innerWidth!=window.undefined) return window.innerWidth;
-        if (document.compatMode=='CSS1Compat') return document.documentElement.clientWidth;
-        if (document.body) return document.body.clientWidth;
-        return window.undefined;
-    }
-
-    function centerPopWin() {
-        var popMask = document.getElementById(MODAL_DIV_ID);
-
-        var width = popMask.offsetWidth;
-        var height = popMask.offsetHeight;
-
-        var fullHeight = getViewportHeight();
-        var fullWidth = getViewportWidth();
-        var scLeft,scTop;
-        if (self.pageYOffset) {
-            scLeft = self.pageXOffset;
-            scTop = self.pageYOffset;
-        } else if (document.documentElement && document.documentElement.scrollTop) {
-            scLeft = document.documentElement.scrollLeft;
-            scTop = document.documentElement.scrollTop;
-        } else if (document.body) {
-            scLeft = document.body.scrollLeft;
-            scTop = document.body.scrollTop;
-        }
-
-        popMask.style.height = fullHeight + "px";
-        popMask.style.width = fullWidth + "px";
-        popMask.style.top = scTop + "px";
-        popMask.style.left = scLeft + "px";
-
-        //check that user's screen is big enough for auto centering...
-        if (fullHeight > height) {
-            popMask.style.top = (scTop + ((fullHeight - height) / 2)) + "px";
-        }
-        if (fullWidth > width) {
-            popMask.style.left =  (scLeft + ((fullWidth - width) / 2)) + "px";
-        }
-    }
-
-    function addEvent(obj, evType, fn) {
-        if (obj.addEventListener) {
-            obj.addEventListener(evType, fn, false);
-            return true;
-        } else if (obj.attachEvent) {
-            var r = obj.attachEvent("on" + evType, fn);
-            return r;
-        } else {
-            return false;
+    /**
+     Remove modal window overlay
+     */
+    function removeOverlay() {
+        if (document.getElementById(MODAL_DIV_ID)) {
+            document.body.removeChild(document.getElementById(MODAL_DIV_ID));
         }
     }
 
     function addShowStatsButton() {
-        var button = document.createElement("button");
-        document.body.appendChild(button);
-        button.id = 'robobruinStatBtn';
-        button.innerHTML = "Show Freebie Stats!";
+        if (document.getElementById(STAT_BUTTON_ID)) {
+            return;
+        }
+        var nodes = xpath(document, "//a[contains(@href,'stattracker')]");
 
-        button.addEventListener('click',
-                           function(e) { showOverlay(); },
-                           false);
-        GM_addStyle('#robobruinStatBtn {position:fixed;top:80px;left:80px;z-index:200;background-color:#0781C8;color:#fff;}');
+        if (nodes.snapshotItem(0)) {
+            var a = nodes.snapshotItem(0);
+            a.href = '#';
+            a.target = null;
+            a.innerHTML = BUTTON_LABEL;
+            a.id = STAT_BUTTON_ID;
+            a.addEventListener('click', function(e) {getStats(); return false;},false);
+        }
+        else {
+            //left this here just in case stattracker button disappears
+            var button = document.createElement("button");
+            document.body.appendChild(button);
+            button.id = STAT_BUTTON_ID;
+            button.innerHTML = BUTTON_LABEL;
+            button.addEventListener('click', function(e) {getStats();}, false);
+            GM_addStyle('#' + STAT_BUTTON_ID + '{position:fixed;top:80px;right:80px;z-index:200;background-color:#0781C8;color:#fff;}');
+        }
     }
 
-    addEvent(window, "resize", centerPopWin);
-    addEvent(window, "scroll", centerPopWin);
-
-    var nodes = xpath(document, "//table[@id='statTable0']/tbody/tr/td[@class='player']/div/a[@class='name' and @href]");
-    processFantasyHome(nodes);
-
+    function getStats() {
+        var nodes = xpath(document, "//table[@id='statTable0']/tbody/tr/td[@class='player']/div/a[@class='name' and @href]");
+        processFantasyHome(nodes);
+    }
+    addShowStatsButton();
 })();
