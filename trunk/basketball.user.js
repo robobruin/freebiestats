cce3fa59595da59d573b1256f189dd543fb604f1
robobruin <robobruin@11e3c3e4-fc19-0410-be34-8bb59711dbe6>
2007-11-03 10:38:54 +0000

    sum up totals and maintain original order for players

--- trunk/basketball.user.js
+++ trunk/basketball.user.js
@@ -37,41 +37,25 @@ revision
     const MODAL_DIV_ID = 'robobruinModal';
     const STAT_BODY_ID = 'robobruinTableBody';
     const BUTTON_LABEL   = 'Show Freebie Stats!';
-
     const STAT_BUTTON_ID = 'robobruinStatBtn';
-//
-//    function Baller() {
-//        this._playerId = '';
-//        this._name     = '';
-//        this._displayName = '';
-//        this._position = '';
-//        this._min = 0;
-//        this._fga = 0;
-//        this._fgm = 0;
-//        this._tpa = 0;
-//        this._tpm = 0;
-//        this._fta = 0;
-//        this._ftm = 0;
-//        this._oreb = 0;
-//        this._reb = 0;
-//        this._assists = 0;
-//        this._to = 0;
-//        this._steals = 0;
-//        this._blk = 0;
-//        this._pf = 0;
-//        this._pts = 0;
-//    }
-//    Baller.prototype.isOnBench   = function ()    {return (this._position == 'BN' || this._position == 'DL');}
-//
-//    var TotalBaller = new Baller();
 
-
+    function Baller() {
+        this._playerId = '';
+        this._name     = '';
+        this._position = '';
+        this._order = 0;
+    }
+    Baller.prototype.isOnBench   = function ()    {return (this._position == 'BN');}
+    Baller.prototype.playerId   = function (arg) {if (arguments.length) this._playerId  = parseInt(arg); else return this._playerId;}
+    Baller.prototype.name   = function (arg) {if (arguments.length) this._name  = arg; else return this._name;}
+    Baller.prototype.position = function (arg) {if (arguments.length) this._position  = arg; else return this._position;}
+    Baller.prototype.order       = function (arg) {if (arguments.length) this._order = parseInt(arg); else return this._order;}
 
     function xpath(doc, xpath) {
         return doc.evaluate(xpath, doc, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
     }
 
-    function getDocument(url, playerId, position) {
+    function getDocument(url, player) {
         GM_xmlhttpRequest({
             method:"GET",
             url:url,
@@ -84,13 +68,13 @@ revision
                 s = s.replace(/\n/g, ' ');
                 s = s.replace(/^.*<style.*<\/style>/gi, ' ');
                 s = s.replace(/^.*<body[^>]*>(.*)<\/body>.*$/gi, "$1");
-                var row = processBoxscore(s, playerId);
-                printStats(row, position);
+                var row = processBoxscore(s, player);
+                printStats(row, player);
             }
         });
     }
 
-    function processBoxscore(html, playerId) {
+    function processBoxscore(html, player) {
         var div = document.getElementById(HIDDEN_DIV_ID);
         if (!div) {
             div = document.createElement("div");
@@ -100,33 +84,97 @@ revision
         }
 
         div.innerHTML = html;
-        var nodes = xpath(document, "//tr[contains(@class,'ysprow')]/td/a[contains(@href,'" + playerId + "')]");
+        var nodes = xpath(document, "//tr[contains(@class,'ysprow')]/td/a[contains(@href,'" + player.playerId() + "')]");
         var i = nodes.snapshotLength;
 
         if (!i) {
             return null;
         } else {
-            var node = nodes.snapshotItem(i - 1).parentNode.parentNode.cloneNode(true);
-            return node;
+            return nodes.snapshotItem(i - 1).parentNode.parentNode.cloneNode(true);
         }
     }
 
-    function printStats(row, position) {
+    function printStats(row, player) {
+        var statBody = findOrCreateStatBody();
+
+        if (row) {
+            var rows = statBody.getElementsByTagName("TR");
+            var tr = document.createElement("tr").appendChild(row);
+            var td = document.createElement("td");
+            td.innerHTML = player.position();
+            tr.insertBefore(td, tr.firstChild);            
+            statBody.replaceChild(tr, rows[player.order()]);
+            if (player.isOnBench()) {
+                tr.className = 'bench';
+            } else {
+                tr.className = (player.order() % 2 == 0) ? 'even' : 'odd';
+                var totalRow = rows[rows.length-1];
+                updateTotals(totalRow, tr);
+            }
+        }
+    }
+
+    function updateTotals(totalRow, currentRow) {
+        var cells = currentRow.getElementsByTagName("TD");
+        var totalCells = totalRow.getElementsByTagName("TD");
+
+        if (cells.length < 10) {return;}
+
+        if (totalCells.length==0) {
+            for (var i=0; i < cells.length; i++) {
+                totalRow.appendChild(document.createElement("td"));
+            }
+        }
+
+        //index 0 is the position
+        //index 1 is the name
+        for (var i=2; i < cells.length; i++) {
+            totalCells[i].innerHTML = sumValues(totalCells[i].innerHTML, cells[i].innerHTML);
+        }
+    }
+
+    function sumValues(oldValue, newValue) {
+        var i = newValue.indexOf("-");
+        if (i > -1) {
+            var ma = parseMadeAttempt(newValue);
+            var tma = parseMadeAttempt(oldValue);
+            var totalMade = parseInt(tma.made) + parseInt(ma.made);
+            var totalAttempted = parseInt(tma.attempted) + parseInt(ma.attempted);
+
+            var pct = (totalAttempted==0) ? "" : "<br/>" + ((parseFloat(totalMade/totalAttempted).toFixed(3))*100).toFixed(1) + "%";
+            return totalMade + "-" + totalAttempted + pct;
+        } else {
+            return parseInt((oldValue == '' ? 0 : oldValue)) + parseInt(newValue);
+        }
+    }
+
+    function parseMadeAttempt(s) {
+        var made = 0;
+        var attempted = 0;
+
+        var i = s.indexOf("-");
+        if (i>-1) {
+            var re = new RegExp("\\D+(\\d+-\\d+).+", 'gi');
+            s = s.replace(re, "$1");
+            i = s.indexOf("-");
+            made = s.substring(0,i);
+            attempted = s.substring(i+1,s.length);
+        }
+        return {made:made,attempted:attempted};
+    }
+
+    function findOrCreateStatBody() {
         var statBody = document.getElementById(STAT_BODY_ID);
         if (!statBody) {
             statBody = addModalOverlay();
             addShowStatsButton();
-        }
 
-        if (row) {
-            var tr = document.createElement("tr").appendChild(row);
-            var className = (statBody.childNodes.length % 2 == 0) ? 'odd' : 'even';
-            tr.className = className;
-            if (position == 'BN') {
-                tr.className = 'bench';
-            }
-            statBody.appendChild(tr);
+            var totalRow = document.createElement("tr");
+            totalRow.className = 'total';
+            statBody.appendChild(totalRow);
+
         }
+        return statBody;
     }
 
     function addModalOverlay() {
@@ -140,14 +188,14 @@ revision
 
         div.innerHTML =
         '<div><table id="roboTable" width="100%">' +
-            '<thead><tr><td width="18%" height="18" align="left">&nbsp;Name</td><td width="6%">Min</td><td width="8%">FG</td><td width="8%">3Pt</td><td width="8%">FT</td><td width="8%">Off</td><td width="6%">Reb</td><td width="6%">Ast</td><td width="6%">TO</td><td width="6%">Stl</td><td width="6%">Blk</td><td width="6%">PF</td><td width="8%">Pts&nbsp;</td></tr></thead>' +
+            '<thead><tr><td width="6%">Pos</td><td width="18%" height="18" align="left">&nbsp;Name</td><td width="6%">Min</td><td width="8%">FG</td><td width="8%">3Pt</td><td width="8%">FT</td><td width="8%">Off</td><td width="6%">Reb</td><td width="6%">Ast</td><td width="6%">TO</td><td width="6%">Stl</td><td width="6%">Blk</td><td width="6%">PF</td><td width="8%">Pts&nbsp;</td></tr></thead>' +
             '<tbody id="'+ STAT_BODY_ID+'">' +
             '</tbody>' +
         '</table></div>';
 
         GM_addStyle('#' + MODAL_DIV_ID + " {position: absolute;left: 0px;top: 0px;width:100%;height:100%;text-align:center;z-index: 200;background: url(\"data:image/png,%89PNG%0D%0A%1A%0A%00%00%00%0DIHDR%00%00%002%00%00%002%01%03%00%00%00%24%F1%1A%F2%00%00%00%06PLTE%9D%BF%C4%FF%FF%FFo%99%7C%D4%00%00%00%02tRNS%FF%00%E5%B70J%00%00%00%01bKGD%01%FF%02-%DE%00%00%00%09pHYs%00%00%00H%00%00%00H%00F%C9k%3E%00%00%00yIDATx%01%05%C1%01%01%00%00%08%02%20%1C%D9I%07u%A2%13A%06%5C%0A6%03.%05%9B%01%97%82%CD%80K%C1f%C0%A5%603%E0R%B0%19p)%D8%0C%B8%14l%06%5C%0A6%03.%05%9B%01%97%82%CD%80K%C1f%C0%A5%603%E0R%B0%19p)%D8%0C%B8%14l%06%5C%0A6%03.%05%9B%01%97%82%CD%80K%C1f%C0%A5%603%E0R%B0%19p)%D8%0C%B8%14l%06%5C%0A%F6%01%90%ADD%F3%BDe%02%17%00%00%00%00IEND%AEB%60%82\");}");
         GM_addStyle('#' + MODAL_DIV_ID + ' div {width:700px;margin: 100px auto;background-color:#fff;border:1px solid #000;padding:15px;text-align:center;z-index:201;}');
-        GM_addStyle('#roboTable {padding:3px;border-collapse:collapse;border: 1px solid #000;} tr.even {background-color:#f1f2ed;} thead tr {background-color:#ABAB9E;border-bottom:1px solid #000;} td {text-align:center;} tr.bench {background-color: #666;}');
+        GM_addStyle(GM_addStyle('.roboTable {width:100%;margin-bottom:20px;padding:3px;border-collapse:collapse;border: 1px solid #000;} tr.odd {background-color:white;font-weight:bold;} tr.even {background-color:beige;font-weight:bold;} thead tr {background-color:#ABAB9E;border-bottom:1px solid #000;} td {text-align:center;} tr.bench {background-color:#f1f2ed;font-weight:normal;} tr.total {background-color:yellow;font-weight:bold}'));
         addCloseAndRefresh();
         return document.getElementById(STAT_BODY_ID);
     }
@@ -175,6 +223,7 @@ revision
     }
 
     function processFantasyHome(nodes) {
+        var numActivePlayers = 0;
         for (var i = 0; i < nodes.snapshotLength; i++) {
             var userIdNode = nodes.snapshotItem(i);
             var playerId = userIdNode.getAttribute('href');
@@ -191,8 +240,23 @@ revision
                     if (column.childNodes.length && column.childNodes[0].nodeName == 'A') {
                         var boxscoreLink = column.childNodes[0].getAttribute("href");
                         boxscoreLink = new String(boxscoreLink).replace('recap', 'boxscore');
+                        if (boxscoreLink.indexOf("preview")> -1) {break;}
+                        //update score link on management page to go directly to boxscore instead of recap
                         column.childNodes[0].setAttribute("href", boxscoreLink);
-                        getDocument(boxscoreLink, playerId, position);
+
+                        var statBody = findOrCreateStatBody();
+                        var rows = statBody.getElementsByTagName("TR");
+                        var lastRow = rows[rows.length-1];
+                        var statRow = document.createElement("tr");
+                        statBody.insertBefore(statRow, lastRow);
+
+                        var player = new Baller();
+                        player.playerId(playerId);
+                        player.position(position);
+                        player.order(numActivePlayers);
+
+                        getDocument(boxscoreLink, player);
+                        numActivePlayers++;
                     }
                     break;
                 }
